1bda8941783b37eae6f7c02cdd836fac5026c23c
fazaamal <83562902+fazaamal@users.noreply.github.com>
2023-09-14 07:40:03 +0000

    Sync LeetCode submission - Minimum Number of Operations to Move All Balls to Each Box (python3)

--- problems/minimum_number_of_operations_to_move_all_balls_to_each_box/solution.py
+++ problems/minimum_number_of_operations_to_move_all_balls_to_each_box/solution.py
@@ -1,13 +1,20 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        ans = [0]*len(boxes)
-        leftCount, leftCost, rightCount, rightCost, n = 0, 0, 0, 0, len(boxes)
-        for i in range(1, n):
-            if boxes[i-1] == '1': leftCount += 1
-            leftCost += leftCount # each step move to right, the cost increases by # of 1s on the left
-            ans[i] = leftCost
-        for i in range(n-2, -1, -1):
-            if boxes[i+1] == '1': rightCount += 1
-            rightCost += rightCount
-            ans[i] += rightCost
-        return ans
+        a = [0]*len(boxes)
+
+        counter = [0, 0]
+        for i in range(len(boxes)):
+            a[i] += sum(counter)
+            counter[0] = sum(counter)
+            if(boxes[i] == '1'):
+                counter[1] += 1
+
+        counter = [0, 0]
+
+        for i in range(len(boxes)-1, -1, -1): 
+            a[i] += sum(counter)
+            counter[0] = sum(counter)
+            if(boxes[i] == '1'):
+                counter[1] += 1
+
+        return a
